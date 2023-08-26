ebc8391b6b6b0b7aa1864b6be43622b8f2a89725
omarihab99 <omar.ihab.mohamed99@gmail.com>
2023-08-27 00:17:14 +0300

    Tested

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\GenreController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,6 +19,23 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::resource('authors', AuthorController::class);
-Route::resource('books', BookController::class);
-Route::resource('genres', GenreController::class);
+// Authors routes
+Route::get('/authors', [AuthorController::class, 'index']);
+Route::get('/authors/{author}', [AuthorController::class, 'show']);
+Route::post('/authors', [AuthorController::class, 'store']);
+Route::put('/authors/{author}', [AuthorController::class, 'update']);
+Route::delete('/authors/{author}', [AuthorController::class, 'destroy']);
+
+// Books routes
+Route::get('/books', [BookController::class, 'index']);
+Route::get('/books/{book}', [BookController::class, 'show']);
+Route::post('/books', [BookController::class, 'store']);
+Route::put('/books/{book}', [BookController::class, 'update']);
+Route::delete('/books/{book}', [BookController::class, 'destroy']);
+
+// Genres routes
+Route::get('/genres', [GenreController::class, 'index']);
+Route::get('/genres/{genre}', [GenreController::class, 'show']);
+Route::post('/genres', [GenreController::class, 'store']);
+Route::put('/genres/{genre}', [GenreController::class, 'update']);
+Route::delete('/genres/{genre}', [GenreController::class, 'destroy']);
